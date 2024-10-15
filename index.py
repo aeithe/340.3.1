ec979cd536f016e413558ec640350de2427b24aa
pisareva.alisa@gmail.com <pisareva.alisa@gmail.com>
2024-10-15 08:33:44 -0400

    first commit

---
+++ index.py
@@ -0,0 +1,23 @@
+stringy = "Everything is an Object in Pyhton!!"
+print(stringy)
+print(stringy.upper())
+print(stringy.lower())
+print(stringy.capitalize())
+
+Famous = "Valerie"
+
+Quote = "Patience, young grasshopper"
+
+print(f'{Famous} oonce  said, "{Quote}."')
+print("{} once said, '{}.'".format(Famous, Quote))
+
+
+stringy = "You're a wizard Harry!"
+
+print(stringy[::-1])
+print(stringy[-1])
+print(stringy[::2])
+print(len(stringy))
+print(stringy[3])
+print(stringy[3:9])
+print(stringy[6:])
